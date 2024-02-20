48244d344a52e8434a2c9b5e32b9009f74157f67
islem bhar <islembhar19970709@gmail.com>
2024-02-20 11:25:04 +0000

    commit leaflet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+import L from "leaflet";
+import "./App.css";
+import LeafletGeocoder from "./LeafletGeocoder";
+import LeafletRoutingMachine from "./LeafletRoutingMachine";
 
 function App() {
+  const position = [36.8065, 10.1815];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/*  <LeafletGeocoder /> */}
+        <LeafletRoutingMachine />
+      </MapContainer>
     </div>
   );
 }
 
+let DefaultIcon = L.icon({
+  iconUrl: "/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 export default App;
